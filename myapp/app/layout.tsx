4efe3b206ae17e1ac638b1a553d02e577adcaa66
unknown <l.ricardlissard@gmail.com>
2026-06-25 15:39:48 +0200

    fix(header): correction du header pour qu'il s'affiche sur toutes les pages

--- myapp/app/layout.tsx
+++ myapp/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Link from "next/link";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Dev",
@@ -14,7 +16,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-      <body>{children}</body>
+      <body><header style={{ backgroundColor: "black" }}>
+        <nav>
+          <Link href="/">
+            <Image src="/logo.svg" alt="DEV" width={95} height={24} priority />
+          </Link>
+          <Link href="/profil">
+            <Image src="/login.svg" alt="Profil" width={24} height={24} />
+          </Link>
+        </nav>
+      </header>{children}</body>
     </html>
   );
 }
--- myapp/app/page.tsx
+++ myapp/app/page.tsx
@@ -4,17 +4,6 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <header style={{ backgroundColor: "black" }}>
-        <nav>
-          <Link href="/">
-            <Image src="/logo.svg" alt="DEV" width={95} height={24} priority />
-          </Link>
-          <Link href="/profil">
-            <Image src="/login.svg" alt="Profil" width={24} height={24} />
-          </Link>
-        </nav>
-      </header>
-
       <section>
         <h1>Nos dernières opportunités</h1>
         <Link href="/offres">{"Voir les offres d'emploi"}</Link>
